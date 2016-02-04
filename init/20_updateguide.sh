b7fb6f0fd0bd837d68cf261373da97687031e31f
johnodon <johnodon@hotmail.com>
2016-02-04 15:01:38 -0500

    Update 20_updateguide.sh

--- init/20_updateguide.sh
+++ init/20_updateguide.sh
@@ -3,5 +3,5 @@ if [ -f "/config/zap2xml.conf1" ]; then
 /zap2xml/zap2xml.pl -C /config/zap2xml.conf1
 /zap2xml/zap2xml.pl -C /config/zap2xml.conf2
 sleep 120
-
+python /zap2xml/xmlcombine.py /data/guide1.xml /data/guide2.xml > /data/guide.xml
 fi

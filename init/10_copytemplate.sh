e446bb20f9ff10c44aba82c71ea8f68cb2b8cb67
johnodon <johnodon@hotmail.com>
2016-02-04 14:58:11 -0500

    Update 10_copytemplate.sh

--- init/10_copytemplate.sh
+++ init/10_copytemplate.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-if [ ! -f "/config/templates/zap2xml.conf.template" ]; then
+if [ ! -f "/config/templates/zap2xml.conf1" ]; then
 mkdir -p /config/templates
-cp /confs/zap2xml.conf.template /config/templates/zap2xml.conf.template
+cp /confs/zap2xml.conf1 /config/templates/zap2xml.conf1
+cp /confs/zap2xml.conf2 /config/templates/zap2xml.conf2
 fi
 chown -R nobody:users /config
 chmod -R 777 /config

7753eae6627b8e2502d780ab0bd0c8fa19d7db78
johnodon <johnodon@hotmail.com>
2016-02-05 11:33:53 -0500

    Update updateguide.sh

--- files/init/updateguide.sh
+++ files/init/updateguide.sh
@@ -1,6 +1,7 @@
+#!/bin/sh
+
 #Update guide data (only if user created .conf files)
 if [ -f "/config/zap2xml.conf1"  -a  -f "/config/zap2xml.conf2" ]; then
-#if [ -f "/config/zap2xml.conf1" ]; then
 	/zap2xml/scripts/zap2xml.pl -C /config/zap2xml.conf1
 	/zap2xml/scripts/zap2xml.pl -C /config/zap2xml.conf2
 	sleep 120

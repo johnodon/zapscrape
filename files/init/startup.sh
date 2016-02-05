4d7e1dd148cfb58a570244180c6de3013e05c522
johnodon <johnodon@hotmail.com>
2016-02-05 09:20:45 -0500

    Update startup.sh

--- files/init/startup.sh
+++ files/init/startup.sh
@@ -1,8 +1,8 @@
 #Copy zap2xml.conf templates to /config/templates
 #if [ ! -d "/config/templates/" ]; then
-	mkdir -p /config/templates
-	cp /zap2xml/templates/zap2xml.conf1 /config/templates/zap2xml.conf1
-	cp /zap2xml/templates/zap2xml.conf2 /config/templates/zap2xml.conf2
+mkdir -p /config/templates
+cp /zap2xml/templates/zap2xml.conf1 /config/templates/zap2xml.conf1
+cp /zap2xml/templates/zap2xml.conf2 /config/templates/zap2xml.conf2
 #else
 #	cp /zap2xml/templates/zap2xml.conf1 /config/templates/zap2xml.conf1
 #	cp /zap2xml/templates/zap2xml.conf2 /config/templates/zap2xml.conf2

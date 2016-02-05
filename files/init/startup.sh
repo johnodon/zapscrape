6b6e84f27b64c7b4d61c03b1fb765776035f002d
johnodon <johnodon@hotmail.com>
2016-02-05 08:38:36 -0500

    Create startup.sh

---
+++ files/init/startup.sh
@@ -0,0 +1,12 @@
+#Copy zap2xml.conf templates to /config/templates
+if [ ! -d "/config/templates/" ]; then
+	mkdir -p /config/templates
+	cp /zap2xml/templates/zap2xml.conf1 /config/templates/zap2xml.conf1
+	cp /zap2xml/templates/zap2xml.conf2 /config/templates/zap2xml.conf2
+else
+	cp /zap2xml/templates/zap2xml.conf1 /config/templates/zap2xml.conf1
+	cp /zap2xml/templates/zap2xml.conf2 /config/templates/zap2xml.conf2
+fi
+
+#set proper unRAID permissions on /config
+chown -R nobody:users /config

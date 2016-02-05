83bbcac37317607de26bc2f690404e63d52b8a41
johnodon <johnodon@hotmail.com>
2016-02-05 10:22:41 -0500

    Update startup.sh

--- files/init/startup.sh
+++ files/init/startup.sh
@@ -20,3 +20,15 @@ if [ ! -f "/config/mycron" ]; then
 fi
 sleep 10
 crontab -u nobody /config/mycron
+
+#Update guide data (only if user created .conf files)
+if [ -f "/config/zap2xml.conf1" ] %% [ -f "/config/zap2xml.conf2" ]; then
+	/zap2xml/scripts/zap2xml.pl -C /config/zap2xml.conf1
+	/zap2xml/scripts/zap2xml.pl -C /config/zap2xml.conf2
+	sleep 120
+	python /zap2xml/scripts/xmlcombine.py /data/guide1.xml /data/guide2.xml > /data/guide.xml   #merges xmls
+fi
+
+#re-set proper unRAID permissions on /config
+chown -R nobody:users /config
+chmod -R 777 /config

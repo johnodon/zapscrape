2adc1a2ca98cab72e7247d63c0b12039ce5897db
johnodon <johnodon@hotmail.com>
2016-02-05 11:33:06 -0500

    Create updateguide.sh

---
+++ files/init/updateguide.sh
@@ -0,0 +1,8 @@
+#Update guide data (only if user created .conf files)
+if [ -f "/config/zap2xml.conf1"  -a  -f "/config/zap2xml.conf2" ]; then
+#if [ -f "/config/zap2xml.conf1" ]; then
+	/zap2xml/scripts/zap2xml.pl -C /config/zap2xml.conf1
+	/zap2xml/scripts/zap2xml.pl -C /config/zap2xml.conf2
+	sleep 120
+	python /zap2xml/scripts/xmlcombine.py /data/guide1.xml /data/guide2.xml > /data/guide.xml   #merges xmls
+fi

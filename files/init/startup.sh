ea0a12edc4587fc32950ce1badd3e8d8745192c7
johnodon <johnodon@hotmail.com>
2016-02-05 13:35:55 -0500

    Update startup.sh

--- files/init/startup.sh
+++ files/init/startup.sh
@@ -4,14 +4,12 @@
 chown -R nobody:users /config
 chmod -R 777 /config
 
-#Copy zap2xml.conf templates to /config/templates
+#Copy zap2xml.conf template to /config/templates
 if [ ! -d "/config/templates/" ]; then
 	mkdir -p /config/templates
-  	cp /zap2xml/templates/zap2xml.conf1 /config/templates/zap2xml.conf1
-  	cp /zap2xml/templates/zap2xml.conf2 /config/templates/zap2xml.conf2
+  	cp /zap2xml/templates/zap2xml.conf /config/templates/zap2xml.conf
 else
-	cp /zap2xml/templates/zap2xml.conf1 /config/templates/zap2xml.conf1
-	cp /zap2xml/templates/zap2xml.conf2 /config/templates/zap2xml.conf2
+	cp /zap2xml/templates/zap2xml.conf /config/templates/zap2xml.conf
 fi
 
 #Add cron job to update guide data
@@ -22,11 +20,8 @@ sleep 10
 crontab -u nobody /config/mycron
 
 #Update guide data (only if user created .conf files)
-if [ -f "/config/zap2xml.conf1"  -a  -f "/config/zap2xml.conf2" ]; then
-	/zap2xml/scripts/zap2xml.pl -C /config/zap2xml.conf1
-	/zap2xml/scripts/zap2xml.pl -C /config/zap2xml.conf2
-	sleep 120
-	python /zap2xml/scripts/xmlcombine.py /data/guide1.xml /data/guide2.xml > /data/guide.xml   #merges xmls
+if [ -f "/config/zap2xml.conf" ]; then
+	/zap2xml/scripts/zap2xml.pl -C /config/zap2xml.conf
 fi
 
 #re-set proper unRAID permissions on /config

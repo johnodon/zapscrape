f662165cc6e6dc27afe76772d7dd5595dfaae791
johnodon <johnodon@hotmail.com>
2016-02-04 19:48:06 -0500

    Update 30_setupcron.sh

--- init/30_setupcron.sh
+++ init/30_setupcron.sh
@@ -1,3 +1,8 @@
-if [ -e /config/mycron ]; then
-  crontab -u nobody /config/mycron
+#!/bin/bash
+if [ ! -e "/config/mycron/" ]; then
+cp /cron/mycron /config/mycron
+chown -R nobody:users /config
+chmod -R 777 /config
+crontab -u nobody /config/mycron
 fi
+

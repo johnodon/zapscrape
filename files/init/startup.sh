c731558514cebdf54f636755f0e81c12a60be4ee
johnodon <johnodon@hotmail.com>
2016-02-05 08:56:17 -0500

    Update startup.sh

--- files/init/startup.sh
+++ files/init/startup.sh
@@ -1,12 +1,12 @@
 #Copy zap2xml.conf templates to /config/templates
-if [ ! -d "/config/templates/" ]; then
+#if [ ! -d "/config/templates/" ]; then
 	mkdir -p /config/templates
 	cp /zap2xml/templates/zap2xml.conf1 /config/templates/zap2xml.conf1
 	cp /zap2xml/templates/zap2xml.conf2 /config/templates/zap2xml.conf2
-else
-	cp /zap2xml/templates/zap2xml.conf1 /config/templates/zap2xml.conf1
-	cp /zap2xml/templates/zap2xml.conf2 /config/templates/zap2xml.conf2
-fi
+#else
+#	cp /zap2xml/templates/zap2xml.conf1 /config/templates/zap2xml.conf1
+#	cp /zap2xml/templates/zap2xml.conf2 /config/templates/zap2xml.conf2
+#fi
 
 #set proper unRAID permissions on /config
-chown -R nobody:users /config
+#chown -R nobody:users /config

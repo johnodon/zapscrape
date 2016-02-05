af6a15245f3db2cbc363b38af334779e92b29488
johnodon <johnodon@hotmail.com>
2016-02-05 11:20:07 -0500

    Update startup.sh

--- files/init/startup.sh
+++ files/init/startup.sh
@@ -22,8 +22,8 @@ sleep 10
 crontab -u nobody /config/mycron
 
 #Update guide data (only if user created .conf files)
-#if [[ -f "/config/zap2xml.conf1" && -f "/config/zap2xml.conf2" ]]; then
-if [ -f "/config/zap2xml.conf1" ]; then
+if [ -f "/config/zap2xml.conf1"  -a  -f "/config/zap2xml.conf2" ]; then
+#if [ -f "/config/zap2xml.conf1" ]; then
 	/zap2xml/scripts/zap2xml.pl -C /config/zap2xml.conf1
 	/zap2xml/scripts/zap2xml.pl -C /config/zap2xml.conf2
 	sleep 120

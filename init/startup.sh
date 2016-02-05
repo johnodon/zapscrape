3ac270ca2d24061ac1f1d508f69bc252bf18a731
johnodon <johnodon@hotmail.com>
2016-02-05 06:42:50 -0500

    Update startup.sh

--- init/startup.sh
+++ init/startup.sh
@@ -1,15 +1,21 @@
+#set proper unRAID permissions on /config
+chown -R nobody:users /config
+chmod -R 777 /config
+sleep 10
+
 #Copy zap2xml templates to /config/templates
 if [ ! -d "/config/templates/" ]; then
 mkdir -p /config/templates
 fi
 cp /confs/zap2xml.conf1.template /config/templates/zap2xml.conf1.template
 cp /confs/zap2xml.conf2.template /config/templates/zap2xml.conf2.template
+sleep 10
 
 #Add cron job to update guide data
 if [ ! -f "/config/mycron" ]; then
 cp /cron/mycron /config/mycron
 fi
-sleep 5
+sleep 10
 crontab -u nobody /config/mycron
 
 #Update guide data (only if user created .conf files)

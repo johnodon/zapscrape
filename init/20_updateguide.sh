c466df622afd044f6d28f0b4389c8acedd2c784c
johnodon <johnodon@hotmail.com>
2016-02-05 06:31:25 -0500

    Update and rename 20_updateguide.sh to updateguide.sh

--- init/20_updateguide.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ -f "/config/zap2xml.conf1" ]; then
-/zap2xml/zap2xml.pl -C /config/zap2xml.conf1
-/zap2xml/zap2xml.pl -C /config/zap2xml.conf2
-sleep 120
-python /zap2xml/xmlcombine.py /data/guide1.xml /data/guide2.xml > /data/guide.xml
-fi
---
+++ init/updateguide.sh
@@ -0,0 +1,7 @@
+#Update guide data
+if [ -f "/config/zap2xml.conf1" ] && if [ -f "/config/zap2xml.conf2" ]; then
+/zap2xml/zap2xml.pl -C /config/zap2xml.conf1
+/zap2xml/zap2xml.pl -C /config/zap2xml.conf2
+sleep 120
+python /zap2xml/xmlcombine.py /data/guide1.xml /data/guide2.xml > /data/guide.xml   #merges guide data
+fi

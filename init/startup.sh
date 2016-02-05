4cf7afb4cde1984d071c3a09edb6cde9d13d045b
johnodon <johnodon@hotmail.com>
2016-02-05 06:23:36 -0500

    Update startup.sh

--- init/startup.sh
+++ init/startup.sh
@@ -5,7 +5,12 @@ fi
 cp /confs/zap2xml.conf1.template /config/templates/zap2xml.conf1.template
 cp /confs/zap2xml.conf2.template /config/templates/zap2xml.conf2.template
 
-
+#Add cron job to update guide data
+if [ ! -f "/config/mycron" ]; then
+cp /cron/mycron /config/mycron
+fi
+sleep 5
+crontab -u nobody /config/mycron
 
 
 chown -R nobody:users /config

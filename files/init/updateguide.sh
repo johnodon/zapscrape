75bc91988436999c42aa33a92a8b9b3abb3294fa
johnodon <johnodon@hotmail.com>
2016-02-06 07:15:11 -0500

    Update updateguide.sh

--- files/init/updateguide.sh
+++ files/init/updateguide.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-#re-set proper unRAID permissions on /config
-chown -R nobody:users /config /data
+#Set proper unRAID permissions on /config
+chown -R nobody:users /config
+chown -R nobody:users /data
 chmod -R 777 /config /data
+chmod -R 777 /config /config
 
 #Update guide data (only if user created .conf files)
 if [ -f "/config/zap2xml.conf1"  -a  -f "/config/zap2xml.conf2" ]; then
@@ -12,6 +14,8 @@ if [ -f "/config/zap2xml.conf1"  -a  -f "/config/zap2xml.conf2" ]; then
 	python /zap2xml/scripts/xmlcombine.py /data/guide1.xml /data/guide2.xml > /data/guide.xml   #merges xmls
 fi
 
-#re-set proper unRAID permissions on /config
-chown -R nobody:users /config /data
+#Re-set proper unRAID permissions on /config
+chown -R nobody:users /config
+chown -R nobody:users /data
 chmod -R 777 /config /data
+chmod -R 777 /config /config

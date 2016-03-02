43b4fd3b2311a382eadb677cb9093a8ccb85db25
johnodon <johnodon@hotmail.com>
2016-03-02 09:31:57 -0500

    Update startup.sh

--- files/init/startup.sh
+++ files/init/startup.sh
@@ -6,29 +6,29 @@ chown -R nobody:users /data
 chmod -R 777 /config /data
 chmod -R 777 /config /config
 
-#Copy zap2xml.conf templates to /config/templates
+#Copy zapscrape.conf templates to /config/templates
 if [ ! -d "/config/templates/" ]; then
 	mkdir -p /config/templates
-  	cp /zap2xml/templates/zap2xml.conf1 /config/templates/zap2xml.conf1
-  	cp /zap2xml/templates/zap2xml.conf2 /config/templates/zap2xml.conf2
+  	cp /zapscrape/templates/zapscrape.conf1 /config/templates/zapscrape.conf1
+  	cp /zapscrape/templates/zapscrape.conf2 /config/templates/zapscrape.conf2
 else
-	cp /zap2xml/templates/zap2xml.conf1 /config/templates/zap2xml.conf1
-	cp /zap2xml/templates/zap2xml.conf2 /config/templates/zap2xml.conf2
+	cp /zapscrape/templates/zapscrape.conf1 /config/templates/zapscrape.conf1
+	cp /zapscrape/templates/zapscrape.conf2 /config/templates/zapscrape.conf2
 fi
 
 #Add cron job to update guide data
 if [ ! -f "/config/mycron" ]; then
-	cp /zap2xml/cron/mycron /config/mycron
+	cp /zapscrape/cron/mycron /config/mycron
 fi
 sleep 10
 crontab -u nobody /config/mycron
 
 #Update guide data (only if user created .conf files)
-if [ -f "/config/zap2xml.conf1"  -a  -f "/config/zap2xml.conf2" ]; then
-	/zap2xml/scripts/zap2xml.pl -C /config/zap2xml.conf1
-	/zap2xml/scripts/zap2xml.pl -C /config/zap2xml.conf2
+if [ -f "/config/zapscrape.conf1"  -a  -f "/config/zapscrape.conf2" ]; then
+	/zapscrape/scripts/zapscrape.pl -C /config/zapscrape.conf1
+	/zapscrape/scripts/zapscrape.pl -C /config/zapscrape.conf2
 	sleep 120
-	python /zap2xml/scripts/xmlcombine.py /data/guide1.xml /data/guide2.xml > /data/guide.xml   #merges xmls
+	python /zapscrape/scripts/xmlcombine.py /data/guide1.xml /data/guide2.xml > /data/guide.xml   #merges xmls
 fi
 
 #re-set proper unRAID permissions on /config

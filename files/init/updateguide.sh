853cde294777cc53342d793fcac0de8124248249
johnodon <johnodon@hotmail.com>
2016-03-02 06:39:03 -0500

    Update updateguide.sh

--- files/init/updateguide.sh
+++ files/init/updateguide.sh
@@ -7,8 +7,8 @@ chmod -R 777 /config /data
 chmod -R 777 /config /config
 
 #Update guide data (only if user created .conf file)
-if [ -f "/config/zap2xml.conf" ]; then
-	/zap2xml/scripts/zap2xml.pl -C /config/zap2xml.conf
+if [ -f "/config/zapscrape.conf" ]; then
+	/zapscrape/scripts/zapscrape.pl -C /config/zapscrape.conf
 fi
 
 #Re-set proper unRAID permissions on /config

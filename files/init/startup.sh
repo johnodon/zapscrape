b71cfe3934b9d7142b13b4daddea6a0a857b6406
johnodon <johnodon@hotmail.com>
2016-03-02 06:38:25 -0500

    Update startup.sh

--- files/init/startup.sh
+++ files/init/startup.sh
@@ -6,24 +6,24 @@ chown -R nobody:users /data
 chmod -R 777 /config /data
 chmod -R 777 /config /config
 
-#Copy zap2xml.conf template to /config/templates
+#Copy zapscrape.conf template to /config/templates
 if [ ! -d "/config/templates/" ]; then
 	mkdir -p /config/templates
-  	cp /zap2xml/templates/zap2xml.conf /config/templates/zap2xml.conf
+  	cp /zapscrape/templates/zapscrape.conf /config/templates/zapscrape.conf
 else
-	cp /zap2xml/templates/zap2xml.conf /config/templates/zap2xml.conf
+	cp /zapscrape/templates/zapscrape.conf /config/templates/zapscrape.conf
 fi
 
 #Add cron job to update guide data
 if [ ! -f "/config/mycron" ]; then
-	cp /zap2xml/cron/mycron /config/mycron
+	cp /zapscrape/cron/mycron /config/mycron
 fi
 sleep 10
 crontab -u nobody /config/mycron
 
 #Update guide data (only if user created .conf file)
-if [ -f "/config/zap2xml.conf" ]; then
-	/zap2xml/scripts/zap2xml.pl -C /config/zap2xml.conf
+if [ -f "/config/zapscrape.conf" ]; then
+	/zapscrape/scripts/zapscrape.pl -C /config/zapscrape.conf
 fi
 
 #Re-set proper unRAID permissions on /config
